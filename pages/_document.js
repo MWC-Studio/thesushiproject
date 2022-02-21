f15f1d14099943fe0215a5136c5931a45afa3d65
Hannah Lim <hjeong.lim@gmail.com>
2022-02-20 20:34:20 -0800

    added font family link

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,7 @@ export default class MyDocument extends Document {
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+            <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap" rel="stylesheet">
           />
         </Head>
         <body>
