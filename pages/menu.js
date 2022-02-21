9686b2eb83e37ef837b0ad22daf600c23b8afbdc
Hannah Lim <hjeong.lim@gmail.com>
2022-02-21 12:31:16 -0800

    add section2 component

--- pages/menu.js
+++ pages/menu.js
@@ -1,11 +1,13 @@
 import TopBanner from "@components/TopBanner";
-import TitlebarBelowImageList from "@components/menu/TitlebarBelowImageList"
-import BottomMenuImageList from "@components/menu/BottomMenuImageList"
+import TitlebarBelowImageList from "@components/menu/TitlebarBelowImageList";
+import BottomMenuImageList from "@components/menu/BottomMenuImageList";
+import CenterDetailedMenuList from "@components/menu/CenterDetailedMenuList";
 const MenuPage = () => {
   return (
     <>
       <TopBanner title="Menu" description="Menu" />
       <TitlebarBelowImageList />
+      <CenterDetailedMenuList />
       <BottomMenuImageList />
     </>
   );
