ea725c78683c80ad6475dd1ace3766a7a5f9662d
jiyunnoh <ianjy1127@gmail.com>
2022-02-22 15:55:53 -0600

    Update OrderBanner
    Update passing bgcolor props

--- pages/menu.js
+++ pages/menu.js
@@ -1,9 +1,11 @@
 import TopBanner from "@components/TopBanner";
 import TitlebarBelowImageList from "@components/menu/TitlebarBelowImageList"
+import OrderBanner from "@components/OrderBanner";
 
 const MenuPage = () => {
   return (
     <>
+      <OrderBanner bgColor="common.white" />
       <TopBanner title="Menu" description="Menu" />
       <TitlebarBelowImageList />
     </>
