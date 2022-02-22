958f1644941693f9bcf761161d66d4080b4c634a
hayeun <whitehy@gmail.com>
2022-02-22 12:15:57 -0800

    Edit Findus.jsx BottomMenuImageList.jsx theme.js

--- components/menu/Findus.jsx
+++ components/menu/Findus.jsx
@@ -1,48 +1,82 @@
 import { Container, Grid, Typography } from "@mui/material";
-import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
 import MuiNextLink from "@components/MuiNextLink";
 import Button from "@mui/material/Button";
+import OrderAppIcon1 from "../../public/icons/doordash.svg";
+import OrderAppIcon2 from "../../public/icons/grubhub.svg";
+import OrderAppIcon3 from "../../public/icons/yelp.svg";
 
 export default function Findus() {
     return (
 
-<Box sx={{ py: 5}} >
-    <Typography
-         sx={{fontSize: 'h1.fontSize',textAlign:"center"}}
+//<Box sx={{ py: 5}} >
+<Grid
+component="section"
+container
+justifyContent="center"
+alignItems="center"
+sx={{
+    height: "29vw",
+    width: "100vw",
+    //palette 업데이트하고 바꾸기.
+    pt: 5,
+    pb: 5
+    // mb: 15,
+  }}
+>
+    <Grid item xs={12}>
+       <Typography
+         variant="h1"
+         align="center"
+         fontWeight={'fontWeightBold'}
+         fontFamily={'Nunito'}
         >People Can Find Us Here
-    </Typography>
-    <Stack direction="row" justifyContent="center" spacing={4} sx={{ mt:5, mb: 5 }}>
-        <MuiNextLink
-    sx={{ textDecoration: "none", color: "common.white" }}
-    href="https://www.doordash.com/"
-    target="_blank"
-    rel="noopener noreferrer"
-         >
-         <img src="/menu_page/doordash.png" />
-        </MuiNextLink>
-        <MuiNextLink
-    sx={{ textDecoration: "none", color: "common.white" }}
-    href="https://www.grubhub.com/"
-    target="_blank"
-    rel="noopener noreferrer"
-        >
-        <img src="/menu_page/grubhub.png" />
-        </MuiNextLink>
-         <MuiNextLink
-    sx={{ textDecoration: "none", color: "common.white" }}
-    href="https://www.yelp.com/"
-    target="_blank"
-    rel="noopener noreferrer"
-         >
-        <img src="/menu_page/yelp.png" />
-        </MuiNextLink>
-    </Stack>
-    <Grid align="center">
+        </Typography>
+    </Grid>
+    
+    <Grid item xs={7}
+                container
+                justifyContent="center"
+                alignItems="center"
+    >
+        <Grid item align="center" xs={3}>
+            <MuiNextLink
+            sx={{ textDecoration: "none", color: "common.white" }}
+            href="https://www.doordash.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+             >
+            <OrderAppIcon1 />
+            </MuiNextLink>
+        </Grid>
+        <Grid item align="center" xs={3}>
+            <MuiNextLink
+            sx={{ textDecoration: "none", color: "common.white" }}
+            href="https://www.grubhub.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+               >
+            <OrderAppIcon2 />
+            </MuiNextLink>
+        </Grid>
+        <Grid item align="center" xs={1}>
+            <MuiNextLink
+            sx={{ textDecoration: "none", color: "common.white" }}
+            href="https://www.yelp.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+             >
+            <OrderAppIcon3 />
+            </MuiNextLink>
+        </Grid>
+   </Grid>
+
+    <Grid item xs={12} align="center">
         <Button variant="contained">
            View Store Menu
         </Button>
     </Grid>
-</Box>
+</Grid>
+
 );
 };
+
