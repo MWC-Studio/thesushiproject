fb8b3db9cdd0bb00dac4e724160d10e1054a5a6d
Kim-Jeongyoung <jeongyoung2@gmail.com>
2022-02-22 02:09:12 -0800

    Edit align images

--- components/About.jsx
+++ components/About.jsx
@@ -6,117 +6,99 @@ import Image from 'next/image';
 
 const AboutPage = () => {
   return (
-    <Container maxwidth="lg">
-      <Box sx={{ mt: 2, mb: 2 }}>
+    <Container
+      justify="center"
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      xs={12}
+      md={6}
+    >
+      <Grid
+        container
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+      >
         <Grid
-          container
           item
           flexDirection="column"
           justifyContent="center"
           alignItems="center"
-          sx={{ pt: 2, pb: 2 }}
+          xs={12}
+          flexDirection="column"
+          justifyContent="center"
+          sx={{ mb: 2, mt: 2 }}
         >
-          <Typography textAlign="center" color="background: #F48A88">
-            {`Quick, Approachable, but keeping the Quality`}
+          <Typography textAlign="center" color="#F48A88" gutterBottom>
+            Quick & Approachable while Keeping the Quality
           </Typography>
-          <Typography
-            component="h2"
-            variant="h4"
-            textAlign="center"
-            gutterBottom
-            mt={2}
-          >
-            That's How We're Rollin'
+          {/* font size = 36px */}
+          <Typography textAligh="center" variant="header1" gutterBottom>
+            That’s How We’re Rollin’
           </Typography>
         </Grid>
-      </Box>
-      <Box></Box>
-      <Box sx={{ mt: 2, mb: 2 }}>
-        <Grid
-          container
-          item
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-          sx={{ pt: 2, pb: 2 }}
-        >
+        <Grid item>
           <Image
-            src="/about-page/about_img_outline.svg"
+            src="/aboutpage/about_img_outline.svg"
             alt="Cooking"
-            layout="responsive"
-            width={588}
-            height={426}
+            width="588"
+            height="426"
           />
-          <Typography
-            component="h2"
-            variant="h4"
-            textAlign="center"
-            gutterBottom
-            mt={2}
-          >
-            {/* It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a more-or-less normal
-            distribution of letters, as opposed to using 'Content here, content
-            here', making it look like readable English. Many desktop publishing
-            packages and web page editors now use Lorem Ipsum as their default
-            model text, and a search for 'lorem ipsum' will uncover many web
-            sites still in their infancy. Various versions have evolved over the
-            years, sometimes by accident, sometimes on purpose (injected humour
-            and the like). */}
+          <Typography variant="body1" textAlign="center">
+            Founded in 2019, Sushi Rollin’ has started with a chef who Chef Kim,
+            also the CEO of
           </Typography>
         </Grid>
-      </Box>
-      <Box sx={{ mt: 2, mb: 2 }}>
-        <Grid
-          container
-          item
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-          sx={{ pt: 2, pb: 2 }}
-        >
+        <Grid item>
           <Image
-            src="/about-page/ceo_frame.svg"
-            alt="Ceo"
-            layout="responsive"
-            width={606}
-            height={546}
+            src="/aboutpage/ceo_frame.svg"
+            alt="CEO"
+            // layout="responsive"
+            width="606"
+            height="546"
           />
+          <Typography variant="body1" textAlign="center">
+            “When people think about Sushi, It’s pricey and not an everyday kind
+            of experience. But with Sushi Rollin, the experience gets more
+            approachable but still unique." -The Chef CEO, June Kim-
+          </Typography>
         </Grid>
-      </Box>
-
-      <Box sx={{ mt: 2, mb: 2 }}>
-        <Grid
-          container
-          item
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-          sx={{ pt: 2, pb: 2 }}
-        >
-          <MuiNextLink href="/menu" underline="none">
-            <Button variant="fill" size="large">
-              Franchise Oppurtunity
-            </Button>
-          </MuiNextLink>
+        <Grid item>
+          <Container
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Box>
+              <MuiNextLink href="/franchise" underline="none">
+                <Image
+                  src="/aboutpage/button_franchise.svg"
+                  alt="Francise oppurtunity"
+                  width="264"
+                  height="55"
+                />
+                {/* <Button variant="fill" size="large">
+                Franchise Oppurtunity
+              </Button> */}
+              </MuiNextLink>
+            </Box>
+            <Box>
+              <MuiNextLink href="/franchise" underline="none">
+                <Image
+                  src="/aboutpage/button_franchise.svg"
+                  alt="Francise oppurtunity"
+                  width="264"
+                  height="55"
+                />
+                {/* <Button variant="outlined" size="large">
+                View Our Menu
+              </Button> */}
+              </MuiNextLink>
+            </Box>
+          </Container>
         </Grid>
-        <Grid
-          container
-          item
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-          sx={{ pt: 2, pb: 2 }}
-        >
-          <MuiNextLink href="/menu" underline="none">
-            <Button variant="outlined" size="large">
-              View Our Menu
-            </Button>
-          </MuiNextLink>
-        </Grid>
-      </Box>
-      <Box>a</Box>
+      </Grid>
     </Container>
   );
 };
@@ -216,7 +198,8 @@ export default AboutPage;
 {
   /* 가운데 정렬 */
 }
-<Grid
+{
+  /* <Grid
   container
   item
   flexDirection="column"
@@ -227,4 +210,5 @@ export default AboutPage;
   justifyContent="center"
 >
   a
-</Grid>;
+</Grid>; */
+}
