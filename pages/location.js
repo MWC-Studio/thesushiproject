683009d0099b70470d2bca4de5def37bcc3ae27c
jiyunnoh <ianjy1127@gmail.com>
2022-02-22 21:03:27 -0600

    Update location.js

--- pages/location.js
+++ pages/location.js
@@ -6,7 +6,7 @@ import OrderBanner from "@components/OrderBanner";
 const LocationPage = () => {
   return (
     <>
-      <TopBanner title="Location" description="Location" />
+      <TopBanner title="Locations" description="Locations" />
       <QuiltedImages />
       <LocationInfoSection />
       <OrderBanner bgColor="common.beige" text="Call for Pickup or Get it Delivered now!" />
