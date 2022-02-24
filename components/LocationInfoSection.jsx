e87e5f673116c3da7899eedfcf54bcd34c7e5dfe
Yangsook Kim <yangsook.kim@gmail.com>
2022-02-23 16:06:45 -0800

    locationInfoSection modified with theme.js

--- components/LocationInfoSection.jsx
+++ components/LocationInfoSection.jsx
@@ -7,68 +7,69 @@ import Image from "next/image";
 const LocationInfoSection = () => {
   return (
     <Container maxWidth="lg" sx={{ my: 15, bgcolor: "", padding: 1, width: 1100 }}>
-        <Grid container spacing={1} sx={{ bgcolor: "", width: 1043}}>
+        <Grid container spacing={1} sx={{ bgcolor: "", width: 1043 }}>
 
             <Grid container  spacing={0} justifyContent="top" sx={{ minHeight: 420, bgcolor: "" }} xs={12} md={6}>
                 <Grid item spacing={0} sx={{ height: 40, bgcolor: "" }} xs={12}>
                     <Image src={'/icons/logodot2.svg'} width={31} height={31}  />
                     <Typography 
                         color="primary"
-                        variant="header1"
+                        variant="h2"
                         align="left"
                         fontWeight={'fontWeightBold'}
                         fontFamily={'Nunito'}
                         ml={1}
+                        sx={{ display: "inline" }}
                     >
                     Sushi Rollin' Garland
                     </Typography>
                 </Grid> 
                 <Grid container spacing={0} sx={{ paddingY: 3,  bgcolor: "" }} xs={12}>
-                    <Grid item xs={12} sx={{ paddingY: 1}}>
+                    <Grid item xs={12} sx={{ paddingY: 2}}>
                         <Typography 
                             color="primary"
-                            variant="header2"
+                            variant="h3"
                             align="left"
                             fontFamily={'Nunito'}
                             fontWeight={'fontWeightBold'}
                         >
                         Contact
                         </Typography>
-                        <Typography variant="body1" color="text.secondary">
+                        <Typography variant="body1" color="text.secondary" sx={{ mt: 1}}>
                         6850 N Shiloh Rd, Garland, TX 75044
                         </Typography>
                         <Typography variant="body1" color="text.secondary">
                         (469) 798-7831
                         </Typography>
                     </Grid> 
-                    <Grid item xs={12} sx={{ paddingY: 1}}>
+                    <Grid item xs={12} sx={{ paddingY: 2}}>
                         <Typography 
                             color="primary"
-                            variant="header2"
+                            variant="h3"
                             align="left"
                             fontWeight={'fontWeightBold'}
                             fontFamily={'Nunito'}
                         >
                         Hours of Operation
                         </Typography>
-                        <Typography variant="body1" color="text.secondary">
+                        <Typography variant="body1" color="text.secondary" sx={{ mt: 1}}>
                         Opens Daily 11 a.m. - 9:30 p.m.
                         </Typography>
                         <Typography variant="body1" color="text.secondary">
                         (Last Call: 9:15 p.m.)
                         </Typography>
                     </Grid> 
-                    <Grid item xs={12} sx={{ paddingY: 1, mb: 10}}>
+                    <Grid item xs={12} sx={{ paddingY: 2, mb: 10}}>
                         <Typography 
                             color="primary"
-                            variant="header2"
+                            variant="h3"
                             align="left"
                             fontWeight={'fontWeightBold'}
                             fontFamily={'Nunito'}
                         >
                         Service Offered
                         </Typography>
-                        <Typography variant="body1" color="text.secondary">
+                        <Typography variant="body1" color="text.secondary" sx={{ mt: 1}}>
                         service : 'Dine in, To-go, Delivery'
                         </Typography>
                     </Grid>
@@ -84,62 +85,63 @@ const LocationInfoSection = () => {
                     <Image src={'/icons/logodot2.svg'} width={31} height={31}  />
                     <Typography 
                         color="primary"
-                        variant="header1"
+                        variant="h2"
                         align="left"
                         fontWeight={'fontWeightBold'}
                         fontFamily={'Nunito'}
                         ml={1}
+                        sx={{ display: "inline" }}
                     >
                     Sushi Rollin' Lewisville
                     </Typography>
                     
                 </Grid> 
                 <Grid container spacing={1} sx={{ paddingY: 3 }} xs={12}>
-                    <Grid item xs={12} sx={{ paddingY: 1}}>
+                    <Grid item xs={12} sx={{ paddingY: 2}}>
                         <Typography 
                             color="primary"
-                            variant="header2"
+                            variant="h3"
                             align="left"
                             fontWeight={'fontWeightBold'}
                             fontFamily={'Nunito'}
                         >
                         Contact
                         </Typography>
-                        <Typography variant="body1" color="text.secondary">
+                        <Typography variant="body1" color="text.secondary" sx={{ mt: 1}}>
                         1288 W Main St #142, Lewisville, TX 75067
                         </Typography>
                         <Typography variant="body1" color="text.secondary">
                         (817) 320-3722
                         </Typography>
                     </Grid> 
-                    <Grid item xs={12} sx={{ paddingY: 1}}>
+                    <Grid item xs={12} sx={{ paddingY: 2}}>
                         <Typography 
                             color="primary"
-                            variant="header2"
+                            variant="h3"
                             align="left"
                             fontWeight={'fontWeightBold'}
                             fontFamily={'Nunito'}
                         >
                         Hours of Operation
                         </Typography>
-                        <Typography variant="body1" color="text.secondary">
+                        <Typography variant="body1" color="text.secondary" sx={{ mt: 1}}>
                         Opens Daily 11 a.m. - 9:00 p.m.
                         </Typography>
                         <Typography variant="body1" color="text.secondary">
                         (Last Call: 8:45 p.m.)
                         </Typography>
                     </Grid> 
-                    <Grid item xs={12} sx={{ paddingY: 1, mb: 10}}>
+                    <Grid item xs={12} sx={{ paddingY: 2, mb: 10}}>
                         <Typography 
                             color="primary"
-                            variant="header2"
+                            variant="h3"
                             align="left"
                             fontWeight={'fontWeightBold'}
                             fontFamily={'Nunito'}
                         >
                         Service Offered
                         </Typography>
-                        <Typography variant="body1" color="text.secondary">
+                        <Typography variant="body1" color="text.secondary" sx={{ mt: 1}}>
                         Dine in, To-go, Delivery
                         </Typography>
                     </Grid>
