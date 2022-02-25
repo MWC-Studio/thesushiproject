07ebd8ba77dc89f82746aa9093d272a31cc099fd
Jiwoo Choi <jchoi136@gmail.com>
2022-02-24 23:54:01 -0800

    create Image Carousel in menu mobile

--- pages/menu.js
+++ pages/menu.js
@@ -3,14 +3,25 @@ import TitlebarBelowImageList from "@components/menu/TitlebarBelowImageList";
 import BottomMenuImageList from "@components/menu/BottomMenuImageList";
 import CenterDetailedMenuList from "@components/menu/CenterDetailedMenuList";
 import OrderBanner from "@components/OrderBanner";
+import CarouselSpecialRoll from "@components/menu/CarouselSpecialRoll";
 
 const MenuPage = (props) => {
   return (
     <>
       <TopBanner title="Menu" description="Menu" isMobile={props.isMobile} />
-      <OrderBanner bgColor="common.white" isMobile={props.isMobile} text="People Can Find Us Here" />
-      <TitlebarBelowImageList />
-      <CenterDetailedMenuList />
+      { props.isMobile ? 
+          <>
+            <CarouselSpecialRoll />
+            <TitlebarBelowImageList />
+            <OrderBanner bgColor="common.white" isMobile={props.isMobile} text="People Can Find Us Here" />
+          </>
+          : 
+          <>
+            <OrderBanner bgColor="common.white" isMobile={props.isMobile} text="People Can Find Us Here" />
+            <TitlebarBelowImageList />
+            <CenterDetailedMenuList />
+          </>
+      }
       <BottomMenuImageList />
     </>
   );
