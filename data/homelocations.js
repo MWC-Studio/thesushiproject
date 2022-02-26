15684a98cf651fb72ab19398b3429cddba21f7f0
YeonheeP <yeonheepark12@gmail.com>
2022-02-25 21:20:17 -0800

    home location images component

--- data/homelocations.js
+++ data/homelocations.js
@@ -1,12 +1,12 @@
 export const section1 = [
     {
         id: 1,
-        img: '../public/home-location-left.svg',
+        img: '/home_page/home-location-left.svg',
         title: 'location image 1'
     },
     {
         id: 2,
-        img: '../public/home-location-right.svg' ,
+        img: '/home_page/home-location-right.svg' ,
         title: 'location image 2'
         
     },
