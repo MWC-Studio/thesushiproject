a574426245c0a059b7b42333acfaa9b3ec98dde9
YeonheeP <yeonheepark12@gmail.com>
2022-02-25 19:28:25 -0800

    homelocation image daga

---
+++ data/homelocations.js
@@ -0,0 +1,15 @@
+export const section1 = [
+    {
+        id: 1,
+        img: '../public/home-location-left.svg',
+        title: 'location image 1'
+    },
+    {
+        id: 2,
+        img: '../public/home-location-right.svg' ,
+        title: 'location image 2'
+        
+    },
+    
+]
+
