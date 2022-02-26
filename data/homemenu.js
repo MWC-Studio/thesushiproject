07427f90961fe419780614fbe1c7bcded4101182
YeonheeP <yeonheepark12@gmail.com>
2022-02-25 19:28:58 -0800

    homemenu menu data

---
+++ data/homemenu.js
@@ -0,0 +1,40 @@
+export const section1 = [
+    {
+        id: 1,
+        name: 'Shrimp Tempura Chef Special Roll',
+        img: '/menu_page/roll0.svg',
+        type: 'Special Rolls'
+    },
+    {
+        id: 2,
+        name: 'Titanic Chef Special Roll',
+        img: '/menu_page/roll1.svg',
+        type: 'Special Rolls'
+    },
+    {
+        id: 3,
+        name: 'Flaming Hot Chef Special Roll',
+        img: '/menu_page/roll2.svg',
+        type: 'Special Rolls'
+    },
+    {
+        id: 4,
+        name: 'Crispy Rice SpicyTuna',
+        img: '/menu_page/roll3.svg',
+        type: 'Special Rolls'
+    },
+    {
+        id: 5,
+        name: 'Tuna Tower Bowl',
+        img: '/menu_page/homemenu-img5.svg',
+        type: 'Special Rolls'
+    },
+    {
+        id: 6,
+        name: 'Poke Bowl',
+        img: '/menu_page/homemenu-img6.svg',
+        description: '',
+        type: 'Special Rolls'
+    }
+]
+
