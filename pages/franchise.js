aeb4afdbe9499c7a5aebdfaf87218d8b728cc97d
jiyunnoh <ianjy1127@gmail.com>
2022-02-25 18:41:45 -0600

    Create textImgGrid in franchise

--- pages/franchise.js
+++ pages/franchise.js
@@ -1,9 +1,11 @@
+import TextImgGrid from "@components/TextImgGrid";
 import TopBanner from "@components/TopBanner";
 
 const FranchisePage = (props) => {
   return (
     <>
       <TopBanner title="Franchise" description="Franchise" isMobile={props.isMobile} />
+      <TextImgGrid />
     </>
   )
 };
