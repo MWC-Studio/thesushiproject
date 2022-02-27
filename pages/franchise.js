be8e52ffdefd8e1d567517912d81e940cdd349c7
jiyunnoh <ianjy1127@gmail.com>
2022-02-26 23:41:56 -0600

    Update whychoose mobile

--- pages/franchise.js
+++ pages/franchise.js
@@ -1,12 +1,13 @@
 import ContactUsBanner from "@components/ContactUsBanner";
 import TextImgGrid from "@components/TextImgGrid";
+import TextImgGridMobile from "@components/TextImgGridMobile";
 import TopBanner from "@components/TopBanner";
 
 const FranchisePage = (props) => {
   return (
     <>
       <TopBanner title="Franchise" description="Franchise" isMobile={props.isMobile} />
-      <TextImgGrid />
+      { props.isMobile ? <TextImgGridMobile /> : <TextImgGrid /> }
       <ContactUsBanner isMobile={props.isMobile} />
     </>
   )
