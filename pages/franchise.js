e5e675cea105ad5f52cb8a098f72e1ac8bfffac2
Yangsook Kim <yangsook.kim@gmail.com>
2022-03-04 11:41:30 -0800

    FranchiseProcess mobile font added

--- pages/franchise.js
+++ pages/franchise.js
@@ -9,7 +9,7 @@ const FranchisePage = (props) => {
     <>
       <TopBanner title="Franchise" description="Franchise" isMobile={props.isMobile} />
       { props.isMobile ? <TextImgGridMobile /> : <TextImgGrid /> }
-      <FranchiseProcess />
+      <FranchiseProcess isMobile={props.isMobile} />
       <ContactUsBanner isMobile={props.isMobile} />
     </>
   )
