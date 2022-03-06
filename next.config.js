ebc84aab536179d7a06e3a1f96dcf2003adfea27
Kim-Jeongyoung <jeongyoung2@gmail.com>
2022-03-05 17:31:07 -0800

    Edit next.config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
+// module.exports = {
+//   reactStrictMode: true,
+//   webpack(config) {
+//     config.module.rules.push({
+//       test: /\.svg$/,
+//       use: '@svgr/webpack',
+//     })
+//     return config
+//   },
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+
+// };
+
 module.exports = {
   reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: '@svgr/webpack',
-    })
-    return config
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
 };
