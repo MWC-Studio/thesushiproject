e2eb7ed75dadddfc6d7f7062f9550520004f54ab
Kim-Jeongyoung <jeongyoung2@gmail.com>
2022-03-06 16:42:32 -0800

    Edit mobile direction at SectionAbout

--- components/home/SectionAbout.jsx
+++ components/home/SectionAbout.jsx
@@ -10,16 +10,13 @@ import Image from 'next/image';
 import MuiNextLink from '@components/MuiNextLink';
 
 const SectionAbout = ({ isMobile }) => {
-  console.log('string', isMobile);
   return (
     <Container component="section" maxWidth="lg" sx={{ my: 10 }}>
-      {/*<Container component="section" maxWidth="md" sx={{ mb: 15 }}>chaged for up spacing*/}
-
       <Grid
         container
-        justifyContent="center"
-        alignItems="center"
         direction={isMobile ? 'column-reverse' : 'row'}
+        alignItems="center"
+        justifyContent="center"
       >
         {/*top text*/}
         <Grid
@@ -40,7 +37,7 @@ const SectionAbout = ({ isMobile }) => {
           >
             About Us
           </Typography>
-          <Typography textAlign="left" sx={{ my: 5, px: 2 }}>
+          <Typography textAlign="center" sx={{ my: 5, px: 2 }}>
             {`Your friendly neighborhood sushi place is here! 
             In Sushi Rollin’ we offer freshness, variety, and authentic experience. A true hole in the wall where you expect something ordinary but walk out with a content belly and heart. We’ve been rolling since 2019.`}
           </Typography>
@@ -53,7 +50,6 @@ const SectionAbout = ({ isMobile }) => {
           <Image
             src="/sectionabout_top.svg"
             alt="about-us"
-            layout="responsive"
             width={800}
             height={600}
           />
@@ -61,12 +57,24 @@ const SectionAbout = ({ isMobile }) => {
       </Grid>
 
       {/*bottom*/}
-      <Grid container justifyContent="center" alignItems="center">
-        <Grid item xs={12} sm={6}>
+      <Grid
+        container
+        direction={isMobile ? 'column' : 'row'}
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          container
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+        >
           <Image
             src="/sectionabout_bottom.svg"
             alt="about-us"
-            layout="responsive"
             width={800}
             height={600}
           />
@@ -89,9 +97,11 @@ const SectionAbout = ({ isMobile }) => {
           >
             Roll with Us
           </Typography>
-          <Typography textAlign="left" sx={{ my: 5, px: 2 }}>
-            {`Sushi Rollin’ started its journey in the middle of Pandemic. Our business model successfully supported delivery focus experience. Over 500+ customer reviews prove that success! Now that we’re expanding, our focus has also evolved to provide a more approachable and enjoyable sushi experience. 
-            It’s an opportunity that keeps rolling. Join us today!  `}
+          <Typography textAlign="center" sx={{ my: 5, px: 2 }}>
+            {`Sushi Rollin’ started its journey in the middle of Pandemic. Our business model successfully supported delivery focus experience. Over 500+ customer reviews prove that success! Now that we’re expanding, our focus has also evolved to provide a more approachable and enjoyable sushi experience.`}
+            <Typography textAlign="left" sx={{ mt: 3, px: 2 }}>
+              {`It’s an opportunity that keeps rolling. Join us today!`}
+            </Typography>
           </Typography>
           <MuiNextLink href="/aboutus" underline="none">
             <Button> Join Us Today</Button>
