b286d20bccb04a21cae0759a79421b74c5810d9f
Hayeun Lee <whitehy@gmail.com>
2022-03-10 23:10:11 -0800

    Update ScrollToTop and Button hover

--- components/menu/ScrollToTop.jsx
+++ components/menu/ScrollToTop.jsx
@@ -1,68 +1,127 @@
-import React, { useState, useEffect } from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
-import IconButton from '@material-ui/core/IconButton';
+import React, { useState, useEffect } from "react";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import IconButton from "@mui/material/IconButton";
 
+const ScrollToTop = ({ showBelow }) => {
 
-const useStyles = makeStyles((theme) => ({
-    toTop: {
-        zIndex: 2,
-        position: 'fixed',
-        bottom: '2vh',
-        color: 'white',
-        backgroundColor: '#F48A88',
-        "&:hover, &.Mui-focusVisible": {
-            transition: '0.3s',
-            color: 'black',
-            backgroundColor: '#F48A88'
-        },
-        [theme.breakpoints.up('xs')]: {
-            right: '3%',
-        },
-        [theme.breakpoints.up('lg')]: {
-            right: '3%',
-        },
+  const [show, setShow] = useState(showBelow ? false : true);
+
+  const handleScroll = () => {
+    if (window.pageYOffset > showBelow) {
+      if (!show) setShow(true);
+    } else {
+      if (show) setShow(false);
     }
-})
-)
+  };
 
+  const handleClick = () => {
+    window[`scrollTo`]({ top: 0, behavior: `smooth` });
+  };
 
-const ScrollToTop = ({
-    showBelow,
-}) => {
-
-    const classes = useStyles();
-
-    const [show, setShow] = useState(showBelow ? false : true)
-
-    const handleScroll = () => {
-        if (window.pageYOffset > showBelow) {
-            if (!show) setShow(true)
-        } else {
-            if (show) setShow(false)
-        }
+  useEffect(() => {
+    if (showBelow) {
+      window.addEventListener(`scroll`, handleScroll);
+      return () => window.removeEventListener(`scroll`, handleScroll);
     }
+  });
 
-    const handleClick = () => {
-        window[`scrollTo`]({ top: 0, behavior: `smooth` })
-    }
+  return (
+    <div>
+      {show && (
+        <IconButton
+          onClick={handleClick}
+          sx={{
+            color: "primary.contrastText",
+            zIndex: 2,
+            position: "fixed",
+            bottom: "2vh",
+            right: "2vh",
+            color: "white",
+            backgroundColor: "#F48A88",
+            "&:hover, &.Mui-focusVisible": {
+              transition: "0.3s",
+              color: "black",
+              backgroundColor: "#F48A88",
+            },
+          }}
+          aria-label="to top"
+          component="span"
+        >
+          <ArrowUpwardIcon />
+        </IconButton>
+      )}
+    </div>
+  );
+};
+export default ScrollToTop;
 
-    useEffect(() => {
-        if (showBelow) {
-            window.addEventListener(`scroll`, handleScroll)
-            return () => window.removeEventListener(`scroll`, handleScroll)
-        }
-    })
 
-    return (
-        <div>
-           {show &&
-                <IconButton 
-                onClick={handleClick} className={classes.toTop} aria-label="to top" component="span">
-                    <ArrowUpwardIcon />
-                </IconButton>
-            }
-        </div>
-    )
-}
-export default ScrollToTop
+
+// import React, { useState, useEffect } from 'react'
+// import { makeStyles } from '@material-ui/core/styles';
+// import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+// import IconButton from '@material-ui/core/IconButton';
+
+
+// const useStyles = makeStyles((theme) => ({
+//     toTop: {
+//         zIndex: 2,
+//         position: 'fixed',
+//         bottom: '2vh',
+//         color: 'white',
+//         backgroundColor: '#F48A88',
+//         "&:hover, &.Mui-focusVisible": {
+//             transition: '0.3s',
+//             color: 'black',
+//             backgroundColor: '#F48A88'
+//         },
+//         [theme.breakpoints.up('xs')]: {
+//             right: '3%',
+//         },
+//         [theme.breakpoints.up('lg')]: {
+//             right: '3%',
+//         },
+//     }
+// })
+// )
+
+
+// const ScrollToTop = ({
+//     showBelow,
+// }) => {
+
+//     const classes = useStyles();
+
+//     const [show, setShow] = useState(showBelow ? false : true)
+
+//     const handleScroll = () => {
+//         if (window.pageYOffset > showBelow) {
+//             if (!show) setShow(true)
+//         } else {
+//             if (show) setShow(false)
+//         }
+//     }
+
+//     const handleClick = () => {
+//         window[`scrollTo`]({ top: 0, behavior: `smooth` })
+//     }
+
+//     useEffect(() => {
+//         if (showBelow) {
+//             window.addEventListener(`scroll`, handleScroll)
+//             return () => window.removeEventListener(`scroll`, handleScroll)
+//         }
+//     })
+
+//     return (
+//         <div>
+//            {show &&
+//                 <IconButton 
+//                 onClick={handleClick} className={classes.toTop} aria-label="to top" component="span">
+//                     <ArrowUpwardIcon />
+//                 </IconButton>
+//             }
+//         </div>
+//     )
+// }
+// export default ScrollToTop
