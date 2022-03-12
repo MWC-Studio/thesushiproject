cf33b6dcd8aeefa6f2ab43ec665146d006e6df05
Kim-Jeongyoung <jeongyoung2@gmail.com>
2022-03-12 14:25:40 -0800

    Add footer width and padding

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,29 +1,21 @@
 import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
-// import { CopyrightTwoTone, Instagram } from '@mui/icons-material';
 import MuiNextLink from '@components/MuiNextLink';
 import { Container, Grid, Typography, useMediaQuery } from '@mui/material';
-
 //Add logo and icons
 import Link from '@mui/material/Link';
-
-// import IconButton from '@mui/material/IconButton';
 import NextLink from 'next/link';
 import Image from 'next/image';
 
-// TODO Need to CLEAN CODE
-
 const Footer = ({ isMobile }) => {
-  // const mobileView = useMediaQuery('(min-width: 375px)');
   return (
     <Container
       component="footer"
       sx={{
-        py: 5,
+        py: 3,
         bgcolor: 'footer.main',
         textDecoration: 'none',
         color: 'common.white',
-        width: '100vw',
+        minWidth: '100vw',
       }}
     >
       <Grid
@@ -33,12 +25,11 @@ const Footer = ({ isMobile }) => {
         alignItems="center"
       >
         {/* Logo */}
-        <Grid item sx={{ align: 'center', mb: 1 }}>
+        <Grid item sx={{ align: 'center', mt: 1, mb: 3 }}>
           <MuiNextLink href="/" underline="none">
             <Image
               src="/footer/footer_rollinlogo_white.svg"
               alt="logo"
-              // layout="responsive"
               width="205"
               height="35"
             />
@@ -56,41 +47,18 @@ const Footer = ({ isMobile }) => {
               alignItems="center"
               flexWrap="wrap"
               px={3}
+              // py={2}
             >
               {/* Contact us */}
               <Grid item sx={{ mt: 2 }}>
                 <Image
                   src="/footer/contactus.svg"
                   alt="email"
-                  // layout="responsive"
                   width="127"
                   height="16"
                 />
 
                 <Box sx={{ mb: 2, mt: 2 }}>
-                  {/* <Link
-                    sx={{ textDecoration: 'none', color: 'common.white' }}
-                    href="mailto:wnsrl91@gmail.com"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Image
-                      src="/footer/email.svg"
-                      alt="email"
-                      // layout="responsive"
-                      width="313"
-                      height="29"
-                    />
-                   
-                  </Link> */}
-
-                  {/* <MuiNextLink
-                    sx={{ textDecoration: 'none', color: 'common.white' }}
-                    href="mailto:wnsrl91@gmail.com"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  > */}
-
                   <Link
                     href="mailto:wnsrl91@gmail.com"
                     target="_blank"
@@ -130,20 +98,47 @@ const Footer = ({ isMobile }) => {
                     >
                       wnsrl91@gmail.com
                     </Typography>
-                    {/* </MuiNextLink> */}
                   </Link>
                 </Box>
                 <Box>
-                  {/* <Link href=""> */}
-                  <Image
-                    src="/footer/phone.svg"
-                    alt="phone number"
-                    // layout="responsive"
-                    width="263"
-                    height="29"
-                  />
-                  {/* <Phone /> */}
-                  {/* </Link> */}
+                  <Link
+                    // href=""
+                    // target="_blank"
+                    // rel="noopener noreferrer"
+                    sx={{
+                      display: 'grid',
+                      gridTemplateColumns: isMobile
+                        ? '20px 40px 1fr'
+                        : '30px 80px 1fr',
+                      columnGap: '12px',
+                      rowGap: '16px',
+                      textDecoration: 'none',
+                      color: 'common.white',
+                    }}
+                  >
+                    <Image
+                      src="/footer/molumen_phone_icon.svg"
+                      alt="phone number"
+                      width="28"
+                      height="28"
+                    />
+                    <Typography
+                      variant={isMobile ? 'body2' : 'h3'}
+                      color="primary.contrastText"
+                      fontWeight="700"
+                      fontFamily="Nunito"
+                    >
+                      Phone
+                    </Typography>
+                    <Typography
+                      variant={isMobile ? 'body2' : 'h3'}
+                      color="primary.contrastText"
+                      fontWeight="400"
+                      fontFamily="Nunito"
+                    >
+                      956-249-0167
+                    </Typography>
+                  </Link>
                 </Box>
               </Grid>
 
@@ -152,11 +147,9 @@ const Footer = ({ isMobile }) => {
                 <Image
                   src="/footer/findus.svg"
                   alt="find us"
-                  // layout="responsive"
                   width="114"
                   height="16"
                 />
-                {/* <Box sx={{ mt: 3 }}>FOLLOW ON US</Box> */}
 
                 <Box sx={{ mb: 2, mt: 2 }}>
                   <Link
@@ -168,26 +161,10 @@ const Footer = ({ isMobile }) => {
                     <Image
                       src="/footer/insta1.svg"
                       alt="Garland Instagram"
-                      // layout="responsive"
                       width="240"
                       height="28"
                     />
                   </Link>
-                  {/* <MuiNextLink
-                    sx={{ textDecoration: 'none', color: 'common.white' }}
-                    href="https://YourInstagramLink/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Instagram fontSize="large" />
-                    <Typography
-                      variant={isMobile ? 'body2' : 'h3'}
-                      fontWeight="700"
-                      fontFamily="Nunito"
-                    >
-                      Sushi Rollin’ Garland
-                    </Typography>
-                  </MuiNextLink> */}
                 </Box>
 
                 <Box sx={{ mb: 2, mt: 2 }}>
@@ -200,56 +177,17 @@ const Footer = ({ isMobile }) => {
                     <Image
                       src="/footer/insta2.svg"
                       alt="Lewisville Instagram"
-                      // layout="responsive"
                       width="258"
                       height="28"
                     />
                   </Link>
                 </Box>
-
-                {/* <Container>
-                  <Stack direction="column" spacing={5} sx={{ mb: 5, py: 2 }}>
-                    <Grid item direction="row">
-                      <MuiNextLink
-                        sx={{ textDecoration: 'none', color: 'common.white' }}
-                        href="https://YourInstagramLink/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <Instagram fontSize="large" />
-                        <Typography
-                          variant={isMobile ? 'body2' : 'h3'}
-                          fontWeight="700"
-                          fontFamily="Nunito"
-                        >
-                          Sushi Rollin’ Garland
-                        </Typography>
-                      </MuiNextLink>{' '}
-                    </Grid>{' '}
-                    <Grid item direction="row"></Grid>
-                    <MuiNextLink
-                      sx={{ textDecoration: 'none', color: 'common.white' }}
-                      href="https://YourInstagramLink/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <Instagram fontSize="large" />
-                      <Typography
-                        variant={isMobile ? 'body2' : 'h3'}
-                        fontWeight="700"
-                        fontFamily="Nunito"
-                      >
-                        Sushi Rollin’ Lewisville
-                      </Typography>
-                    </MuiNextLink>
-                  </Stack>
-                </Container> */}
               </Grid>
             </Grid>
           </Grid>
         </Container>
         {/* Copyright */}
-        <Grid item xs={12} sx={{ mb: 1, mt: 7 }}>
+        <Grid item xs={12} sx={{ mb: 1, mt: 5 }}>
           <Typography
             align="center"
             color="common.white"
