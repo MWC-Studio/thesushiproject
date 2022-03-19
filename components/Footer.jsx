8cb392fe4d3c4868d62919cb08aee15fa729b913
Kim-Jeongyoung <jeongyoung2@gmail.com>
2022-03-19 12:23:47 -0700

    Edit image file to text

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -52,19 +52,13 @@ const Footer = ({ isMobile }) => {
               {/* Contact us */}
               <Grid item sx={{ mt: 2 }}>
                 <Typography
-                  variant={isMobile ? 'body2' : 'h3'}
+                  variant={isMobile ? 'h5' : 'h3'}
                   color="primary.contrastText"
                   fontWeight="700"
                   fontFamily="Nunito"
                 >
                   CONTACT US
                 </Typography>
-                {/* <Image
-                  src="/footer/contactus.svg"
-                  alt="email"
-                  width="127"
-                  height="16"
-                /> */}
 
                 <Box sx={{ mb: 2, mt: 2 }}>
                   <Link
@@ -153,20 +147,14 @@ const Footer = ({ isMobile }) => {
               {/* Find us on */}
               <Grid item sx={{ mt: 5.5 }}>
                 <Typography
-                  variant={isMobile ? 'body2' : 'h3'}
+                  variant={isMobile ? 'h5' : 'h3'}
+                  // variant="h3"
                   color="primary.contrastText"
                   fontWeight="700"
                   fontFamily="Nunito"
                 >
                   FIND US ON
                 </Typography>
-
-                {/* <Image
-                  src="/footer/findus.svg"
-                  alt="find us"
-                  width="114"
-                  height="16"
-                /> */}
 
                 <Box sx={{ mb: 2, mt: 2 }}>
                   <Link
