e72ca3234354fb6287b42f5efd1d6226da81a40d
jiyunnoh <ianjy1127@gmail.com>
2022-03-25 13:02:54 -0500

    Update QuiltedImages to QuiltedImagesWithText

--- pages/locations.js
+++ pages/locations.js
@@ -1,5 +1,5 @@
 import TopBanner from "@components/TopBanner";
-import QuiltedImages from "@components/locations/QuiltedImages";
+import QuiltedImagesWithText from "@components/locations/QuiltedImagesWithText";
 import OrderBanner from "@components/OrderBanner";
 import CarouselMobileImage from "@components/locations/CarouselMobileImage";
 import LocationsContent from "@components/locations/LocationsContent";
@@ -14,8 +14,8 @@ const LocationPage = (props) => {
         description="Locations"
         isMobile={props.isMobile}
       />
-      {props.isMobile ? <CarouselMobileImage /> : <QuiltedImages />}
-      <LocationsContent isMobile={props.isMobile} />
+      {props.isMobile ? <CarouselMobileImage /> : <QuiltedImagesWithText />}
+      {/* <LocationsContent isMobile={props.isMobile} /> */}
         <OrderBanner
           bgColor="common.beige"
           isMobile={props.isMobile}
