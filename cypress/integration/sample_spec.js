0c1453a481275e3cd2a973559acd934443e07fe2
Hannah Lim <hjeong.lim@gmail.com>
2022-03-25 19:07:30 -0700

    add unit test for menu page

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,9 +1,24 @@
-describe('Home page', () => {
+// describe('Home page', () => {
+//   beforeEach(() => {
+//     cy.visit('/');
+//   })
+//   it('Should header have text', () => {
+//     cy.contains('Sushi Experience')
+//     cy.get("header").should("be.exist");
+//   })
+// })
+
+describe("Menu", () => {
   beforeEach(() => {
-    cy.visit('/');
+    cy.visit("/menu");
   })
-  it('Should header have text', () => {
-    cy.contains('Sushi Experience')
-    cy.get("header").should("be.exist");
+  it("has titles", () => {
+    cy.contains('Find Us Here');
+    cy.contains('Menu');
+    cy.contains('Special Rolls');
+    cy.contains('Sushi Rollin\' Experience');
+  })
+  it("has a button", () => {
+    cy.contains('View Store Menu').click();
   })
 })
