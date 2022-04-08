22b06a56c732d2e5a93b1270cef957b53bc9dcb7
Jiwoo Choi <jchoi136@gmail.com>
2022-04-07 21:00:02 -0700

    add ImageListWithTitle component for specialRoll section

--- components/menu/TitlebarBelowImageList.jsx
+++ components/menu/TitlebarBelowImageList.jsx
@@ -1,10 +1,8 @@
 import { Container, Grid, Typography } from "@mui/material";
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
-import ImageListItemBar from "@mui/material/ImageListItemBar";
 import { section1 } from "../../data/menu";
+import ImageListWithTitle from "./ImageListWithTitle";
 
-export default function TitlebarBelowImageList() {
+const TitlebarBelowImageList = () => {
   return (
     <Grid
       component="section"
@@ -19,32 +17,10 @@ export default function TitlebarBelowImageList() {
         <Typography color="text" variant="h2" align="center">
           Special Rolls
         </Typography>
-
-        <ImageList sx={{ p: 7, overflow: "hidden" }} cols={3} gap={33}>
-          {section1.map((item) => (
-            <div key={item.id}>
-              <ImageListItem>
-                <img
-                  src={`${item.img}?w=380&h=380&fit=crop&auto=format`}
-                  srcSet={`${item.img}?w=380&h=380&fit=crop&auto=format&dpr=2 2x`}
-                  alt={item.title}
-                  loading="eager"
-                />
-                <ImageListItemBar position="below" />
-                <Typography
-                  component={"div"}
-                  color="text"
-                  variant="body1"
-                  align="center"
-                  fontWeight={"fontWeightBold"}
-                >
-                  {item.name}
-                </Typography>
-              </ImageListItem>
-            </div>
-          ))}
-        </ImageList>
+        <ImageListWithTitle imgData={section1} />
       </Container>
     </Grid>
   );
-}
+};
+
+export default TitlebarBelowImageList;
