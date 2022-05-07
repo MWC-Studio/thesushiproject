43f41d1b0d82c15842a31dcf4fceae3637de199a
Jiwoo Choi <jchoi136@gmail.com>
2022-05-08 06:42:13 +0900

    create timeline componenent

---
+++ components/aboutus/Timeline.jsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+
+const Timeline = () => {
+  return (
+    <Timeline position="left">
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Eat</TimelineContent>
+      </TimelineItem>
+    </Timeline>
+  );
+};
+
+export default Timeline;
