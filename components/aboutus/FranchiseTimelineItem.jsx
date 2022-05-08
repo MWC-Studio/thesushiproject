1110aba1c2b514330fbcf06bd44ef8b4b1119a52
Jiwoo Choi <jchoi136@gmail.com>
2022-05-08 10:03:47 +0900

    add styling to date and text

--- components/aboutus/FranchiseTimelineItem.jsx
+++ components/aboutus/FranchiseTimelineItem.jsx
@@ -3,15 +3,41 @@ import TimelineItem from "@mui/lab/TimelineItem";
 import TimelineSeparator from "@mui/lab/TimelineSeparator";
 import TimelineConnector from "@mui/lab/TimelineConnector";
 import TimelineContent from "@mui/lab/TimelineContent";
+import Image from "next/dist/client/image";
+import { Typography } from "@mui/material";
+import StepConnector from "@mui/material/StepConnector";
 
 const FranchiseTimelineItem = (props) => {
+  const position = props.position;
+
   return (
-      <TimelineItem position={props.position}>
-        <TimelineSeparator>
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>{props.data.date}{props.data.text}</TimelineContent>
-      </TimelineItem>
+    <TimelineItem position={position}>
+      <TimelineSeparator>
+        <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
+      </TimelineSeparator>
+      <TimelineContent>
+        <div
+          style={{
+            display: "flex",
+          }}
+        >
+          <Image src={"/icons/logo-circle.svg"} width={18} height={18} />
+          <Typography variant="h5" component="span">
+            {props.data.date}
+          </Typography>
+        </div>
+        <ul>
+          <li>
+            <Typography
+              variant="body1"
+              align={position == "left" ? "right" : "left"}
+            >
+              {props.data.text}
+            </Typography>
+          </li>
+        </ul>
+      </TimelineContent>
+    </TimelineItem>
   );
 };
 
